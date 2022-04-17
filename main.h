662ee6ad7015082d2a729b983a5a83437026e249
Reggie071993 <reykakwela@yahoo.com>
2022-04-17 18:09:43 +0000

    main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+/**
+* struct print - structure for printing various types
+* @t: type to print
+* @f: function to print
+*/
+
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
