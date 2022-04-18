2c716dc606d5381d9ba7541e9a9b3f3cac6175f2
Reggie071993 <reykakwela@yahoo.com>
2022-04-18 19:29:58 +0000

    main.h

--- main.h
+++ main.h
@@ -24,17 +24,16 @@ int(*getspecifier(const char *format, int i))(va_list);
 
 int print_c(va_list c);
 int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
 int print_S(va_list S);
-int print_rs(va_list rs);
-int print_rot(va_list ro);
-
-int _print_i(va_list vi);
-int _print_b(va_list b);
-
-int _print_u(va_list);
-int _print_o(va_list);
-int _print_x(va_list);
-int _print_X(va_list);
-int _print_p(va_list);
+int print_r(va_list r);
+int print_R(va_list R);
 
 #endif /* MAIN_H */
