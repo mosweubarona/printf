89046a4a73a6bb400d38d15a4b491dbbcc758602
Reggie071993 <reykakwela@yahoo.com>
2022-04-18 20:53:13 +0000

    main.h

--- main.h
+++ main.h
@@ -6,19 +6,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int print_char(va_list c);
 int _putchar(char c);
 int _printf(const char *format, ...);
-
+int print_string(va_list s);
+int print_int(va_list i);
+int print_dec(va_list d);
+int print_rev(va_list r);
+int print_bin(va_list b);
+int print_unsig(va_list u);
+int print_octal(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_rot13(va_list R);
 /**
-* struct print - structure for printing various types
-* @t: type to printt
-* @f: function to print
-*/
-
-typedef struct print
+  * struct code_format - Struct format
+  *
+  * @sc: The specifiers
+  * @f: The function associated
+  */
+typedef struct code_format
 {
-	char *t;
+	char *sc;
 	int (*f)(va_list);
-} print;
+} code_f;
 
 #endif /* MAIN_H */
