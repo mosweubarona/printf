bfbf48c9a0b88ef5b4d4be3d28758b6f8efa6c55
Reggie071993 <reykakwela@yahoo.com>
2022-04-18 16:44:27 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -13,15 +13,6 @@ static int (*check_for_specifiers(const char *format))(va_list)
 		{"s", print_s},
 		{"i", print_i},
 		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
 		{NULL, NULL}
 	};
 
