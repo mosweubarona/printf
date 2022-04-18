3d0c48cfb42f61db0e41955d1bfb6eca771d2f57
Reggie071993 <reykakwela@yahoo.com>
2022-04-18 17:25:50 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,21 +1,49 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * _printf - Outputs a formatted string.
- * @format: Character string to print - may contain directives.
- *
- */
+* _printf - prints anything
+* @format: pointer to string that contains specifiers
+* Return: number of characters printed
+**/
 
 int _printf(const char *format, ...)
 {
-	buffer_t *output;
-	va_list args;
-		return (-1);
-	output = init_buffer();
-	if (output == NULL)
-		return (-1);
-	ret = run_printf(format, args, output);
+	unsigned int i = 0, count = 0;
+	int (*f)(va_list);
+	va_list list;
 
-	return (ret);
+	if (format == '\0')
+		return (-1);
+	va_start(list, format);
+
+	while (format && format[i])
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+		else if (format[i] == '%' && format[i + 1] == '\0')
+			count++;
+		}
+			return (-1);
+		else if (format[i] == '\0')
+			return (count);
+		else if (format[i] == '%')
+		{
+			f = getspecifier(format, i + 1);
+			i += 1;
+			if (f == NULL)
+			{
+				count += strange(format, i);
+			}
+			else
+			{
+				count = count + f(list);
+				if (format[i] == '+' || format[i] == ' ' || format[i] == '#')
+					i++;
+			}
+		}
+		i++;
+	}
+	va_end(list);
+	return (count);
 }
